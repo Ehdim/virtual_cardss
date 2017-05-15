7bfc017d1830cb8eeb0910d4a8a53271ee83e18d
Ehdim <ehdim1@gmail.com>
2017-05-15 21:27:42 +0400

    Add files via upload

---
+++ Virtual kart/Virtual kart/Kart.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_kart_ns
+{
+    class Kart
+    {
+        private string name;
+        private int hesab_nomresi;
+        private DateTime bitme_vaxti;
+        private int istifade_muddeti;
+        private int cvc;
+        private double mebleg;
+        public string Name
+        {
+            set
+            {
+                this.name=value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public int HesabNomresi
+        {
+            set
+            {
+                this.hesab_nomresi = value;
+            }
+            get
+            {
+                return hesab_nomresi;
+            }
+        }
+    public int IstifadeMuddeti
+        {
+            set
+            {
+                this.istifade_muddeti = value;
+            }
+            get
+            {
+                return istifade_muddeti;
+            }
+        }
+    public int Cvc
+        {
+            set
+            {
+                this.cvc = value;
+            }
+            get
+            {
+                return cvc;
+            }
+        }
+     public double Mebleg
+        {
+            set
+            {
+                this.mebleg = value;
+            }
+            get
+            {
+                return mebleg;
+            }
+        }
+        public string CreateName(string name)
+        {
+            this.Name = name;
+            return name;
+        }
+        public int CreatHesabNomresi()
+        {
+            Random a = new Random();
+            this.HesabNomresi = a.Next(1, 1000000);
+            return this.HesabNomresi;
+        }
+        public int CreateIstifadeMuddeti(int x)
+        {
+            this.istifade_muddeti = x;
+            return x;
+        }
+        public int CreateCvc()
+        {
+            Random cv = new Random();
+            this.Cvc = cv.Next(1, 3);
+            return this.Cvc;
+        }
+        public DateTime CreateBitmeMuddeti(int mounth,int year)
+        {
+            bitme_vaxti = new DateTime(year, mounth, 1);
+           
+            return bitme_vaxti;
+        }
+    }
+}
---
+++ Virtual kart/Virtual kart/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Virtual_kart_ns;
+
+namespace Virtual_kart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Username-i daxil edin");
+            string username = Console.ReadLine();
+            Kart virtual_kart = new Kart();
+            if (username == "Ehdim")
+            {
+                Console.WriteLine(virtual_kart.CreatHesabNomresi());
+            }
+            else
+            {
+               DateTime b= virtual_kart.CreateBitmeMuddeti(1,2017);
+                Console.WriteLine(b);
+                Console.WriteLine("username sehvdir");
+            }
+            Console.ReadLine();
+        }
+    }
+}
